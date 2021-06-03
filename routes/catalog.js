8e028ecf97736904f6f76b26bdb488e861cc522b
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-06-03 16:28:40 +0200

    Add view for specific category: display items

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
+const createError = require('http-errors');
+
 const debug = require('debug')('catalogRouter');
 
 const Category = require('../models/category');
@@ -8,7 +10,7 @@ const Item = require('../models/item');
 
 const currency = require('../models/currency');
 
-router.get('/', async function (req, res, next) {
+router.get('/', async function (req, res) {
   let items, categories;
   try {
     items = await Item.find({});
@@ -17,6 +19,22 @@ router.get('/', async function (req, res, next) {
     debug(err);
   }
   res.render('catalog', { items, currency, categories });
+});
+
+router.get('/category/:id', async function (req, res, next) {
+  let category;
+
+  try {
+    category = await Category.findById(req.params.id);
+    items = await Item.find({ category: req.params.id });
+  } catch (err) {
+    debug(err);
+    return next(createError(400, 'Invalid URL.'));
+  }
+
+  if (category == null) next(createError(404, 'Resource was not found'));
+
+  res.render('category', { currency, category, items });
 });
 
 module.exports = router;
